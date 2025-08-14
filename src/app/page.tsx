c88d34f25422542bd255f4089c5ee27f11a385ca
Dailton Bastos <dailtonbastos@gmail.com>
2025-08-13 23:44:05 -0300

    feat: add logo login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Image from 'next/image';
 import { Login } from '@/components/auth/login';
 
 const Home = async () => {
   return (
     <section className="flex flex-col items-center justify-center h-screen p-4">
-      <div className="flex flex-col items-center justify-center rounded-lg p-6 border border-gray-200 w-full max-w-96 h-48">
-        <h1 className="text-2xl font-medium">Olá, entre na sua conta</h1>
+      <div className="flex flex-col items-center justify-center rounded-lg p-6 border border-gray-200 w-full max-w-96">
+        <div className="flex items-center justify-center mb-4">
+          <Image src="/logo.png" width={80} height={80} alt="Mar" />
+        </div>
+        <h1 className="text-2xl font-medium mb-4">Olá, entre na sua conta</h1>
 
         <Login />
       </div>
